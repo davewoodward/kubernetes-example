91113f6c39fd66c8e67a35feec5d6741e12417db
Dave Woodward <dave@salte.io>
2020-05-25 19:53:31 -0500

    Working, albeit ugly, example.

--- main.tf
+++ main.tf
@@ -1,12 +1,25 @@
+variable "password" {}
+
+locals {
+  username = "admin"
+}
+
 provider "google" {
   version     = "~> 3.22"
+
   credentials = file("account.json")
   project     = "kubernetes-example-278112"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+provider "null" {
+  version     = "~> 2.1"
 }
 
 resource "google_container_cluster" "primary" {
   name     = "my-gke-cluster"
-  location = "us-central1"
+
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
@@ -18,16 +31,19 @@ resource "google_container_cluster" "primary" {
     client_certificate_config {
       issue_client_certificate = true
     }
+    password = var.password
+    username = local.username
   }
-
-  # provisioner "local-exec" {
-  #   command = "gcloud container clusters get-credentials ${google_container_cluster.gke-cluster.name} --zone  ${google_container_cluster.gke-cluster.zone} --project [PROJECT_ID]"
-  # }
 }
+
+# resource "null_resource" "get_credentials" {
+#   provisioner "local-exec" {
+#     command = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --project \"kubernetes-example-278112\""
+#   }
+# }
 
 resource "google_container_node_pool" "primary_preemptible_nodes" {
   name       = "my-node-pool"
-  location   = "us-central1"
   cluster    = google_container_cluster.primary.name
   node_count = 1
 
@@ -50,39 +66,52 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth.0.cluster_ca_certificate)
   host                   = "https://${google_container_cluster.primary.endpoint}"
   load_config_file       = "false"
+  password               = var.password
+  username               = local.username
 }
 
 resource "kubernetes_namespace" "example" {
   metadata {
     name = "my-first-namespace"
   }
+
+  depends_on = [
+    google_container_cluster.primary,
+    google_container_node_pool.primary_preemptible_nodes
+  ]
 }
 
 resource "kubernetes_pod" "test" {
   metadata {
     name = "terraform-example"
     namespace = "my-first-namespace"
+    labels = {
+      app = "TheApp"
+    }
   }
 
   spec {
     container {
-      image = "nginx:1.7.9"
+      image = "nginx:latest"
       name  = "example"
-
-      env {
-        name  = "environment"
-        value = "test"
+      port {
+        container_port = 80
       }
+
+    #   env {
+    #     name  = "environment"
+    #     value = "test"
+    #   }
 
       liveness_probe {
         http_get {
-          path = "/nginx_status"
+          path = "/"
           port = 80
 
-          http_header {
-            name  = "X-Custom-Header"
-            value = "Awesome"
-          }
+          # http_header {
+          #   name  = "X-Custom-Header"
+          #   value = "Awesome"
+          # }
         }
 
         initial_delay_seconds = 3
@@ -90,44 +119,80 @@ resource "kubernetes_pod" "test" {
       }
     }
 
-    dns_config {
-      nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
-      searches    = ["example.com"]
+    # dns_config {
+    #   nameservers = ["1.1.1.1", "8.8.8.8", "9.9.9.9"]
+    #   searches    = ["example.com"]
 
-      option {
-        name  = "ndots"
-        value = 1
-      }
+    #   option {
+    #     name  = "ndots"
+    #     value = 1
+    #   }
 
-      option {
-        name = "use-vc"
-      }
+    #   option {
+    #     name = "use-vc"
+    #   }
+    # }
+
+    # dns_policy = "None"
+  }
+
+
+  depends_on = [
+    google_container_cluster.primary,
+    google_container_node_pool.primary_preemptible_nodes
+  ]
+}
+
+resource "kubernetes_service" "example" {
+  metadata {
+    name = "terraform-example"
+    namespace = "my-first-namespace"
+  }
+
+  spec {
+    selector = {
+      app = "TheApp"
     }
-
-    dns_policy = "None"
+    session_affinity = "ClientIP"
+    port {
+      port        = 80
+      target_port = 80
+    }
+    type = "LoadBalancer"
   }
 }
 
-output ca_certificate {
-  value       = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
-  sensitive   = false
-  description = "Base64 encoded public certificate that is the root of trust for the cluster."
+resource "kubernetes_ingress" "example" {
+  metadata {
+    name = "example-ingress"
+    namespace = "my-first-namespace"
+  }
+
+  spec {
+    backend {
+      service_name = "terraform-example"
+      service_port = 80
+    }
+
+    rule {
+      http {
+        path {
+          backend {
+            service_name = "terraform-example"
+            service_port = 80
+          }
+
+          path = "/*"
+        }
+      }
+    }
+
+    tls {
+      secret_name = "tls-secret"
+    }
+  }
 }
 
-output client_certificate {
-  value       = google_container_cluster.primary.master_auth.0.client_certificate
-  sensitive   = false
-  description = "Base64 encoded public certificate used by clients to authenticate to the cluster endpoint."
-}
-
-output client_key {
-  value       = google_container_cluster.primary.master_auth.0.client_key
-  sensitive   = true
-  description = "Base64 encoded private key used by clients to authenticate to the cluster endpoint."
-}
-
-output endpoint {
-  value       = google_container_cluster.primary.endpoint
-  sensitive   = false
-  description = "The IP address of this cluster's Kubernetes master."
+output "endpoint" {
+  value = kubernetes_ingress.example.load_balancer_ingress
 }
